5050f7fd2d0e88de7c1ebf03a1d4a7ebee84689e
kano1201 <kano.nakamura3@gmail.com>
2021-06-16 04:42:00 +0000

    [Add]bookdetail投稿詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
@@ -14,19 +14,20 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
