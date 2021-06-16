9ac120f4ab8f62df83c53c0c9eb7f147f5737e8b
kano1201 <kano.nakamura3@gmail.com>
2021-06-16 04:23:27 +0000

    [Add]newbook新規投稿画面

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
