015024d9340d1a46ba4413d6ae6e78506059932d
kano1201 <kano.nakamura3@gmail.com>
2021-06-17 05:09:28 +0000

    [Fix]login.signup後の遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
