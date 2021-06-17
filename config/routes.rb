b80ddfe783d001e462eee97f6f1a863661736c64
kano1201 <kano.nakamura3@gmail.com>
2021-06-17 02:43:57 +0000

    [Add]ヘッダーボタン

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'homes' => 'homes#about'
   devise_for :users
   resources :books
   resources :users, only: [:show, :edit, :index, :update]
